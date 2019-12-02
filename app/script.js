70380e0f4db0b7296d5b7742ddecca3ce17a19d1
Marius Malaiska <mariusmalaiska@gmail.com>
2019-12-02 02:36:19 +0200

    mobile

--- app/script.js
+++ app/script.js
@@ -2,15 +2,18 @@ var mySwiper1 = new Swiper('.swiper-container', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
-    slidesPerView: 3,
+    slidesPerView: 1,
     spaceBetween: 120,
     autoplay: {
         delay: 4000,
         disableOnInteraction: true,
     },
     breakpoints: {
-        1000: {
+        1100: {
             slidesPerView: 3
+        },
+        700: {
+            slidesPerView: 2
         },
     },
 
